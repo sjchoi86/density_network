42bcbbbf6d22188f981d1d4ba25ec60eea936b77
root <root@csi-cluster-gpu13.dakao.io>
2018-09-13 03:31:12 +0900

    two types of variance computations

--- src/mdn_class.py
+++ src/mdn_class.py
@@ -65,18 +65,30 @@ class MDN_class(object):
             self.logvar = tf.reshape(self.logvar,shape=(-1,self.y_dim,self.k)) # [n x y_dim x k]
             self.sig_rate = tf.placeholder(shape=[],dtype=tf.float32,name='sig_rate') # [1]
             if self.sig_max == 0:
-                self.var = tf.exp(self.logvar)
+                self.var = tf.exp(self.logvar) # [n x y_dim x k]
             else:
-                self.var = self.sig_max*self.sig_rate*tf.nn.sigmoid(self.logvar)
+                self.var = self.sig_max*self.sig_rate*tf.nn.sigmoid(self.logvar) # [n x y_dim x k]
             self.layers.append(self.logvar)
         # Computations 
-        self.cat = tfd.Categorical(probs=self.pi)            
+        self.cat = tfd.Categorical(probs=self.pi) # categorical r.v.
         self.comps = [tfd.MultivariateNormalDiag(loc=loc,scale_diag=scale) 
                         for loc,scale in zip(tf.unstack(tf.transpose(self.mu,[2,0,1])),
                                              tf.unstack(tf.transpose(self.var,[2,0,1])))]
-        self.tfd_mog = tfd.Mixture(cat=self.cat,components=self.comps)
+        self.tfd_mog = tfd.Mixture(cat=self.cat,components=self.comps) # mixture of Gaussian 
         self.log_liks = self.tfd_mog.log_prob(self.y) # [n]
         self.log_lik = tf.reduce_mean(self.log_liks) # [1]
+        # pi:[n x k] / mu:[n x d x k] / var:[n x d x k]
+        # compute EV and VE
+        # E[Var[y]] - Aleatoric
+        # weighted sum of mu
+        pi_axis = self.pi[:,:,tf.newaxis]
+        self.EVs = tf.squeeze(tf.matmul(self.var,pi_axis),axis=2) # [n x y_dim]
+        self.EV = tf.reduce_sum(self.EVs,axis=1) # [n]
+        # Var[E[y]] - Epistemic 
+        mu_average = tf.matmul(self.mu,self.pi[:,:,tf.newaxis]) # [n x y_dim x 1]
+        mu_diff_sq = tf.square(self.mu-mu_average) # [n x y_dim x k]
+        self.VEs = tf.squeeze(tf.matmul(mu_diff_sq,pi_axis),axis=2) # [n x y_dim]
+        self.VE = tf.reduce_sum(self.VEs,axis=1) # [n]
         # Sampler
         self.y_sample = tf.squeeze(self.tfd_mog.sample(1),[0]) # [n x y_dim]
         # Optimizer
@@ -106,7 +118,7 @@ class MDN_class(object):
                 
     # Plot results
     def plot_result(self,_x_test,_title='MDN result',_fontsize=18,
-                    _figsize=(15,5),_wspace=0.05,_hspace=0.05,_sig_rate=1.0,_pi_th=0.0,
+                    _figsize=(15,5),_wspace=0.1,_hspace=0.05,_sig_rate=1.0,_pi_th=0.0,
                     _x_train=None,_y_train=None,
                     _ylim=[-3,+3]):
         # sample
@@ -114,6 +126,7 @@ class MDN_class(object):
         mu,var,pi = self.sess.run([self.mu,self.var,self.pi],feed_dict={self.x:_x_test,self.sig_rate:_sig_rate})
         # plot per each output dimensions (self.y_dim)
         nr,nc = 1,self.y_dim
+        if nc>2: nc=2 # Upper limit on the number of columns
         gs  = gridspec.GridSpec(nr,nc)
         gs.update(wspace=_wspace, hspace=_hspace)
         fig = plt.figure(figsize=_figsize)
@@ -131,16 +144,45 @@ class MDN_class(object):
                              facecolor='k', interpolate=True, alpha=0.05)            
                 idx = np.where(pi[:,j]>_pi_th)[0]
                 plt.fill_between(_x_test[idx,0],mu[idx,i,j]-2*var[idx,i,j],mu[idx,i,j]+2*var[idx,i,j],
-                             facecolor=colors[j], interpolate=True, alpha=0.3)            
+                             facecolor=colors[j], interpolate=True, alpha=0.3)
             for j in range(self.k): # per each mixture, plot mu
                 idx = np.where(pi[:,j]>_pi_th)[0]
                 plt.plot(_x_test[:,0],mu[:,i,j],'-',color=[0.8,0.8,0.8],linewidth=1)
                 plt.plot(_x_test[idx,0],mu[idx,i,j],'-',color=colors[j],linewidth=3)
+            plt.xlim([_x_test.min(),_x_test.max()])
             plt.ylim(_ylim)
+            plt.xlabel('Input',fontsize=13)
+            plt.ylabel('Output',fontsize=13)
+            plt.title('[%d]-th dimension'%(i+1),fontsize=13)
+        plt.show()
+
+    # Plot
+    def plot_variances(self,_x_test,_title='blue:Var[E[y|x]] / red:E[Var[y|x]]',_fontsize=18,
+                       _figsize=(15,5),_wspace=0.1,_hspace=0.05):
+        # Plot EV and VE
+        VEs,EVs = self.sess.run([self.VEs,self.EVs],feed_dict={self.x:_x_test,self.sig_rate:1.0}) # sig_rate
+        VEs = 0.1*VEs # scale V[E[y|x]] to match that of E[V[y|x]]
+        # plot per each output dimensions (self.y_dim)
+        nr,nc = 1,self.y_dim
+        if nc>2: nc=2 # Upper limit on the number of columns
+        gs  = gridspec.GridSpec(nr,nc)
+        gs.update(wspace=_wspace, hspace=_hspace)
+        fig = plt.figure(figsize=_figsize)
+        fig.suptitle(_title, size=_fontsize)
+        for i in range(nr*nc): # per each dimension
+            ax = plt.subplot(gs[i])
+            plt.plot(_x_test.squeeze(),VEs[:,i],'b-')
+            plt.plot(_x_test.squeeze(),EVs[:,i],'r-')
+            plt.xlim([_x_test.min(),_x_test.max()])
+            plt.xlabel('Input',fontsize=13)
+            plt.ylabel('Output',fontsize=13)
+            plt.title('[%d]-th dimension'%(i+1),fontsize=13)
         plt.show()
         
-    # Train 
-    def train(self,_x_train,_y_train,_x_test,_max_iter=10000,_batch_size=256,_pi_th=0.1,_SHOW_EVERY=10,_figsize=(15,5),_ylim=[-3,+3]):    
+    
+    # Train the mixture of density network
+    def train(self,_x_train,_y_train,_x_test,_max_iter=10000,_batch_size=256,_pi_th=0.1,
+              _SHOW_EVERY=10,_figsize=(15,5),_ylim=[-3,+3]):    
         n_train = _x_train.shape[0]
         for iter in range(_max_iter): 
             iter_rate_1to0 = np.exp(-4*((iter+1.0)/_max_iter)**2)
@@ -155,15 +197,16 @@ class MDN_class(object):
             _,cost_val = self.sess.run([self.optm,self.cost],
                                        feed_dict={self.x:x_batch,self.y:y_batch,
                                                   self.sig_rate:sig_rate})
-            # See progress 
+            # See progress
             if ((iter%(_max_iter//_SHOW_EVERY))==0) | (iter==(_max_iter-1)):
                 # Plot results
                 self.plot_result(_x_test=_x_test,_x_train=_x_train,_y_train=_y_train,
                                  _sig_rate=sig_rate,_pi_th=_pi_th,
                                  _title='[%d/%d] Black dots:training data / Red crosses:samples'%(iter,_max_iter),
                                  _fontsize=18,_figsize=_figsize,_ylim=_ylim)
+                self.plot_variances(_x_test=_x_test,
+                                    _title='blue:Var[E[y|x]] (epistemic) / red:E[Var[y|x]] (aleatoric)',
+                                    _figsize=_figsize)
+                
                 # Print-out
                 print ("[%03d/%d] cost:%.4f"%(iter,_max_iter,cost_val)) 
-        
-
-
