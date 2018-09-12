89c4595a49171f1fdf33545722d1f97559926b26
root <root@csi-cluster-gpu13.dakao.io>
2018-09-12 11:51:11 +0900

    mdn

---
+++ src/util.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import tensorflow as tf
+
+def plot_1d_graphs(_x1_list,_y1_list,_linestyles1,_markers1,_colors1,
+                   _x2_list=None,_y2_list=None,_linestyles2=None,_markers2=None,_colors2=None,
+                   _nR=1,_nC=10,_figsize=(15,2),
+                   _title=None,_titles=None,_tfs=15,
+                   _wspace=0.05,_hspace=0.05):
+    nr,nc = _nR,_nC
+    fig = plt.figure(figsize=_figsize)
+    if _title is not None:
+        fig.suptitle(_title, size=15)
+    gs  = gridspec.GridSpec(nr,nc)
+    gs.update(wspace=_wspace, hspace=_hspace)
+    for i in range(_nR*_nC):
+        ax = plt.subplot(gs[i])
+        plt.plot(_x1_list[i],_y1_list[i],
+                 linestyle=_linestyles1[i],marker=_markers1[i],color=_colors1[i])
+        if _x2_list is not None:
+            plt.plot(_x2_list[i],_y2_list[i],
+                     linestyle=_linestyles2[i],marker=_markers2[i],color=_colors2[i])
+        if _titles is not None:
+            plt.title(_titles[i],size=_tfs)
+    plt.show() 
+    
+    
+def gpu_sess(): 
+    config = tf.ConfigProto(); 
+    config.gpu_options.allow_growth=True
+    sess = tf.Session(config=config)
+    return sess    
